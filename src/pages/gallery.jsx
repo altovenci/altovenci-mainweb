3a588d2cf856dc1fee9210ac63f28bc40cf578ab
altovenci <altovenci.connect@gmail.com>
2026-04-08 01:16:46 +0300

    resolving the conflict of tailwindcss versioning and added zustand

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Gallery = () => {
     return (
         <>
-            <h1>Gallery Page</h1>
+            <h1 className="bg-red-500 text-white p-4">Gallery Page</h1>
         </>
     )
 }
